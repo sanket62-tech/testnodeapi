b230399e4fba75c8088f72f1e14ab7740177c5b1
sanket62-tech <alphasanket62@gmail.com>
2026-02-05 16:25:06 +0530

    chages for village info

--- formSyncApi/src/repositories/formEntry.repository.js
+++ formSyncApi/src/repositories/formEntry.repository.js
@@ -2,6 +2,14 @@ const FormEntry = require('../models/formEntry.model');
 
 const upsertOne = async (doc) => {
   const addedBy = doc.added_by !== undefined && doc.added_by !== null ? parseInt(doc.added_by) : undefined;
+  
+  if (parseInt(doc.form_id) === 290 && addedBy !== undefined && !Number.isNaN(addedBy)) {
+    const filter = { form_id: 290, added_by: addedBy };
+    const update = { $set: doc };
+    const options = { upsert: true, new: true };
+    return FormEntry.findOneAndUpdate(filter, update, options);
+  }
+
   const hasKey = !!doc.record_unique_id && addedBy !== undefined && !Number.isNaN(addedBy);
   if (hasKey) {
     const filter = { record_unique_id: doc.record_unique_id, added_by: addedBy };
